d9891c04943b5ea9d856f39554d21574d4b031d3
AlexandreDresch <z3r0pc@protonmail.com>
2023-10-12 00:46:39 -0300

    feat: create user & create task

---
+++ src/main/java/com/alexandredresch/BeforeIDie/BeforeIDieApplication.java
@@ -0,0 +1,13 @@
+package com.alexandredresch.BeforeIDie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeforeIDieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeforeIDieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexandredresch/BeforeIDie/BeforeIDieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandredresch.BeforeIDie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeforeIDieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
